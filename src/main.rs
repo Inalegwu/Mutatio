b9c07bcf37a5a88d764c119ab034146e854b213f
Ikwue Inalegwu <ikwueinalegwu@gmail.com>
2024-05-03 22:22:44 +0100

    initializing project an getting things setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,52 @@
+use clap::Parser;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::{fs::File, path::Path};
+
+#[derive(Parser, Debug)]
+#[command(version, about,long_about=None)]
+struct Args {
+    file: String,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "lowercase")]
+enum Type {
+    Dark,
+    Light,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+struct TokenColors {
+    scope: Vec<String>,
+    settings: HashMap<String, String>,
+}
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct VsCodeTheme {
+    name: String,
+    r#type: Type,
+    colors: HashMap<String, String>,
+    token_colors: Vec<TokenColors>,
+    semantic_highlighting: bool,
+    semantic_token_colors: HashMap<String, String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+
+    let path = Path::new(&args.file);
+
+    let file = File::open(path);
+
+    match file {
+        Ok(v) => {
+            let theme: VsCodeTheme =
+                serde_json::from_reader(v).expect("Error parsing vs code theme");
+            println!("{:#?}", theme)
+        }
+        Err(e) => {
+            panic!("Error Occurred {}", e);
+        }
+    }
 }
